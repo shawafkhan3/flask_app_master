2395a51ea518a0a4fe7e30a1d4508f3d28b1c840
shawafkhan3 <56125424+shawafkhan3@users.noreply.github.com>
2023-06-17 19:33:46 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,86 @@
+history=[{'role':'user','content':nquestion},{'role':'assistant','content':g[0]}]
+
+def create_search_prompt(chat_history, query):
+  return f'''Your task is to take into consideration two things, one is the chat history that has happend between the User and AI and other is the question. Now you need to 
+modify the question if only it is needed according to chat history and generate  a new question that can searched upon. You have to handle follow up questions and take into 
+considerations the previous responses of the AI if necessary. If the question is not related to the previous responses then output the same question as inputted. If you are not confident on whether the question is related to previous responses, then output the same question.
+
+Example 1:
+------------
+Chat History:
+User: When is MG University established?
+AI: 1998
+
+Given question: then what about Rao University?
+Rephrased question: When is Rao University established?
+------------
+
+Example 2:
+------------
+Chat History:
+User: When is the Republic day of India?
+AI: January 26
+
+Given question: Who is Pawan Kalyan?
+Rephrased question: Who is Pawan Kalyan?
+------------
+
+Example 3:
+------------
+Chat History:
+
+
+Given question: What is water made up of?
+Rephrased question: What is water made up of?
+------------
+
+
+Chat history:
+{chat_history}
+
+Given question: {query}
+Rephrased question: 
+'''
+
+def create_prompt(context, query):
+  return f'''Your task is to extract the answer from the context for the question. You will be given a context and question. Answer the question as truthfully as possible and if the
+answer does not lie in the context then say "I don't know".
+
+Context
+----------
+{context}
+----------
+
+Question: {query}
+Answer: 
+'''
+
+
+
+def get_formatted_history(history):
+  chat_history=''
+  for message in history:
+    if(message['role']=='user'):
+      chat_history+='User: '+message['content']+'\n'
+    elif(message['role']=='assistant'):
+      chat_history+='AI: '+message['content']+'\n'
+  return chat_history.strip()
+
+search_prompt = create_search_prompt(get_formatted_history(history), "What was my previous question")
+
+
+
+response = openai.Completion.create(
+                      engine="text-davinci-003",
+                      prompt=search_prompt,
+                      temperature=0,
+                      max_tokens=400,
+                    #   top_p=0,
+                      frequency_penalty=0.0,
+                      presence_penalty=0.0,
+                    #   stop=["\n"]
+                    )
+
+
+
+response['choices'][0]["text"]
